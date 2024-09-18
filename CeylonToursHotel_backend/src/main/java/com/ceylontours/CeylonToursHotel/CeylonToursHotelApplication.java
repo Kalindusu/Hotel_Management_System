c4caf1d724b55920256a87f836c4d18f05c54dd4
Kalindu Sudaraka <146382563+Kalindusu@users.noreply.github.com>
2024-09-18 12:44:14 +0530

    first commit

---
+++ CeylonToursHotel_backend/src/main/java/com/ceylontours/CeylonToursHotel/CeylonToursHotelApplication.java
@@ -0,0 +1,13 @@
+package com.ceylontours.CeylonToursHotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CeylonToursHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CeylonToursHotelApplication.class, args);
+	}
+
+}
---
+++ CeylonToursHotel_backend/src/test/java/com/ceylontours/CeylonToursHotel/CeylonToursHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ceylontours.CeylonToursHotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CeylonToursHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
