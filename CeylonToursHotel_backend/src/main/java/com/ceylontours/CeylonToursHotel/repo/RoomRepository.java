d82097487d8be77a9daf773821af86689163cbfe
Kalindu Sudaraka <146382563+Kalindusu@users.noreply.github.com>
2024-09-21 00:53:38 +0530

    update

--- CeylonToursHotel_backend/src/main/java/com/ceylontours/CeylonToursHotel/repo/RoomRepository.java
+++ CeylonToursHotel_backend/src/main/java/com/ceylontours/CeylonToursHotel/repo/RoomRepository.java
@@ -2,9 +2,16 @@ package com.ceylontours.CeylonToursHotel.repo;
 
 import com.ceylontours.CeylonToursHotel.entity.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface RoomRepository extends JpaRepository<Room,Long> {
+    @Query("SELECT DISTINCT r.roomType rom Room r")
     List<String> findDistinctRoomTypes();
+
+
+    @Query("SELECT r FROM r WHERE r.id NOT IN(SELECT b.room.id FROM Booking b")
+    List<Room>getAllAvailableRooms();
+
 }
