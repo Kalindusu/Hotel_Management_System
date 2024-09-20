7b93d5e0ac2e6dc21c25a72e6aa85babf948dbd4
Kalindu Sudaraka <146382563+Kalindusu@users.noreply.github.com>
2024-09-20 11:52:03 +0530

    update

--- CeylonToursHotel_backend/src/main/java/com/ceylontours/CeylonToursHotel/entity/Room.java
+++ CeylonToursHotel_backend/src/main/java/com/ceylontours/CeylonToursHotel/entity/Room.java
@@ -1,4 +1,18 @@
 package com.ceylontours.CeylonToursHotel.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="rooms")
 public class Room {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String bookingConfiremationCode;
+    private String roomType;
+    private String roomPrice;
+    private String roomPhotoUrl;
 }
