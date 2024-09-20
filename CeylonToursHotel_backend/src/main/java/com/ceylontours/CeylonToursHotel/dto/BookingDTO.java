6d31612a2b3a77138a8d29f30468403f6fc6a881
Kalindu Sudaraka <146382563+Kalindusu@users.noreply.github.com>
2024-09-20 23:40:58 +0530

    update

---
+++ CeylonToursHotel_backend/src/main/java/com/ceylontours/CeylonToursHotel/dto/BookingDTO.java
@@ -0,0 +1,23 @@
+package com.ceylontours.CeylonToursHotel.dto;
+
+import com.ceylontours.CeylonToursHotel.entity.Room;
+import com.ceylontours.CeylonToursHotel.entity.User;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class BookingDTO {
+    private Long id;
+    private LocalDate checkInData;
+    private LocalDate checkOutData;
+    private int numOfAdult;
+    private int numOfChildren;
+    private int totalNumOfGuest;
+    private String bookingConfiremationCode;
+    private User user;
+    private Room room;
+}
