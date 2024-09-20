9dea127a99de64687eca00c6036a38f3a0a19d0d
Kalindu Sudaraka <146382563+Kalindusu@users.noreply.github.com>
2024-09-20 22:36:48 +0530

    update

--- CeylonToursHotel_backend/src/main/java/com/ceylontours/CeylonToursHotel/entity/Room.java
+++ CeylonToursHotel_backend/src/main/java/com/ceylontours/CeylonToursHotel/entity/Room.java
@@ -3,6 +3,9 @@ package com.ceylontours.CeylonToursHotel.entity;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Data
 @Entity
 @Table(name="rooms")
@@ -11,8 +14,22 @@ public class Room {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String bookingConfiremationCode;
+
     private String roomType;
     private String roomPrice;
     private String roomPhotoUrl;
+    private String roomDescription;
+    private List<Booking> booking =new ArrayList<>();
+
+    @Override
+    public String toString() {
+        return "Room{" +
+                "id=" + id +
+                ", roomType='" + roomType + '\'' +
+                ", roomPrice='" + roomPrice + '\'' +
+                ", roomPhotoUrl='" + roomPhotoUrl + '\'' +
+                ", roomDescription='" + roomDescription + '\'' +
+                ", booking=" + booking +
+                '}';
+    }
 }
