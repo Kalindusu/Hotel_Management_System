1858c173a0db68a7d904d53bab82e940e2306a77
Kalindu Sudaraka <146382563+Kalindusu@users.noreply.github.com>
2024-09-23 22:00:34 +0530

    update

--- CeylonToursHotel_backend/src/main/java/com/ceylontours/CeylonToursHotel/repo/BookingRepository.java
+++ CeylonToursHotel_backend/src/main/java/com/ceylontours/CeylonToursHotel/repo/BookingRepository.java
@@ -7,4 +7,6 @@ import java.util.List;
 
 public interface BookingRepository  extends JpaRepository<Booking, Long> {
     List<Booking> findByRoomId(Long roomId);
+    List<Booking> findByRoomConfirmationCode(String confirmationCode);
+    List<Booking> findByUserId(Long userId);
 }
