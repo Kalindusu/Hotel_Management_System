5e90e4ca679e3d6567dde077f1c33e6689cfbf7f
Kalindu Sudaraka <146382563+Kalindusu@users.noreply.github.com>
2024-09-23 21:56:22 +0530

    update

--- CeylonToursHotel_backend/src/main/java/com/ceylontours/CeylonToursHotel/repo/BookingRepository.java
+++ CeylonToursHotel_backend/src/main/java/com/ceylontours/CeylonToursHotel/repo/BookingRepository.java
@@ -1,4 +1,10 @@
 package com.ceylontours.CeylonToursHotel.repo;
 
-public interface BookingRepository {
+import com.ceylontours.CeylonToursHotel.entity.Booking;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface BookingRepository  extends JpaRepository<Booking, Long> {
+    List<Booking> findByRoomId(Long roomId);
 }
--- CeylonToursHotel_backend/src/main/java/com/ceylontours/CeylonToursHotel/repo/RoomRepository.java
+++ CeylonToursHotel_backend/src/main/java/com/ceylontours/CeylonToursHotel/repo/RoomRepository.java
@@ -10,9 +10,10 @@ import java.util.List;
 public interface RoomRepository extends JpaRepository<Room,Long> {
     @Query("SELECT DISTINCT r.roomType rom Room r")
     List<String> findDistinctRoomTypes();
-@Query("SELECT r FROM Room r WHERE r.roomType LIKE %:roomType% AND r.id NOT IN (SELECT bk.room.id FROM Booking bk WHERE " + "(() )")
+    @Query("SELECT r FROM Room r WHERE r.roomType LIKE %:roomType% AND r.id NOT IN (SELECT bk.room.id FROM Booking bk WHERE" +
+            "(bk.checkInDate <= :checkOutDate) AND (bk.checkOutDate >= :checkInDate))")
 List<Room> findAvailableRoomsByDatesAndTypes(LocalDate checkInDate,LocalDate checkOutDate,String roomType);
-    @Query("SELECT r FROM r WHERE r.id NOT IN(SELECT b.room.id FROM Booking b")
+    @Query("SELECT r FROM Room r WHERE r.id NOT IN (SELECT b.room.id FROM Booking b)")
     List<Room>getAllAvailableRooms();
 
 }
