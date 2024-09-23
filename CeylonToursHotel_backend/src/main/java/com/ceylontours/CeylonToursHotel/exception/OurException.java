8fdaa15d80b5ba6505702be9208e867505efe5c1
Kalindu Sudaraka <146382563+Kalindusu@users.noreply.github.com>
2024-09-23 22:02:31 +0530

    update

---
+++ CeylonToursHotel_backend/src/main/java/com/ceylontours/CeylonToursHotel/exception/OurException.java
@@ -0,0 +1,8 @@
+package com.ceylontours.CeylonToursHotel.exception;
+
+public class OurException extends  RuntimeException {
+
+    public OurException(String message){
+        super(message);
+    }
+}
