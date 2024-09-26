433e323c07ba52c937e8e3627103c8da18e0e2c4
Kalindu Sudaraka <146382563+Kalindusu@users.noreply.github.com>
2024-09-27 00:16:16 +0530

    Controller update

--- CeylonToursHotel_backend/src/main/java/com/ceylontours/CeylonToursHotel/controller/RoomController.java
+++ CeylonToursHotel_backend/src/main/java/com/ceylontours/CeylonToursHotel/controller/RoomController.java
@@ -1,4 +1,10 @@
 package com.ceylontours.CeylonToursHotel.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/rooms")
 public class RoomController {
+
 }
