0b5dcb55205756786e6e5ac554703ebe29ae49d5
Kalindu Sudaraka <146382563+Kalindusu@users.noreply.github.com>
2024-09-28 08:16:15 +0530

    update

--- ceylontours_frontend/src/App.js
+++ ceylontours_frontend/src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import FooterComponent from './component/common/Footer';
 import HomePage from './component/home/HomePage';
 import Navbar from './component/common/Navbar';
@@ -9,12 +9,16 @@ import Navbar from './component/common/Navbar';
 function App() {
   return (
     <div className="App">
+      <Router>
       <Navbar/>
       <div className='content'>
         <Routes>
         <Route exact path="/home" element={<HomePage />} />
         </Routes>
       </div> 
+      </Router>
+      <FooterComponent/>
+      
     </div>
   );
 }
